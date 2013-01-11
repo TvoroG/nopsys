abed01ade74963a6560213d702f94454533d3ce4
Marsel Zaripov <marszaripov@gmail.com>
2013-01-11 16:06:21 +0400

    add some comments

--- boot/stage0.asm
+++ boot/stage0.asm
@@ -1,13 +1,14 @@
 bits 16
-org	0x7c00
+org 0x7c00
 
-NUM		equ		0x11
-DISK	equ		0x00
-POS		equ		0x0002
-HEAD	equ		0x00		
+NUM 	equ		0x11
+DISK 	equ		0x00
+POS 	equ		0x0002
+HEAD 	equ		0x00		
 
 		
-start:	jmp	stage0				; Normalize the start address
+start:
+		jmp	stage0				; Normalize the start address
 		
 stage0:
 		cli						; disable interrupts
@@ -16,19 +17,19 @@ stage0:
 		mov	ss, ax
 		mov	es, ax
 		mov	sp, 0x7c00			; set the stack pointer
-		sti						; enable interrupts
+		sti						; enable intterupts
 
 		;; reset floppy disk(dl = 0) controller
 		mov	ah, 0x00
 		int	0x13
 
 		;; read stage1 sectors from floopy
-		mov al, NUM				; 17 sectors to read
+		mov	al, NUM				; 17 sectors to read
 		mov	cx, POS				; Track/Sector
 		mov	dh, HEAD			; Head
 		mov	bx, 0x1000			; es:bx = 0x1000, buffer
 		mov	ah, 0x02			
-		int 0x13
+		int	0x13
 		
 		jc	hang				; jamp if error
 
@@ -43,7 +44,7 @@ print:	lodsb
 
 		;; TODO jamp to stage1
 hang:
-		jmp hang
+		jmp	hang
 
 		
 msg		db	"ok", 0
